621419895c51da584bb40b3286fc6ae49e2ce356
Aref Azizian <storm.aref@outlook.com>
2020-05-07 21:16:31 +0430

    first commit

---
+++ app/src/main/java/com/sa/factor/MainActivity.java
@@ -0,0 +1,26 @@
+package com.sa.factor;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                timer.cancel();
+                startActivity(new Intent(MainActivity.this, SendActivity.class));
+            }
+        }, 1000, 10000);
+    }
+}
